afb30f3a5d814dff024ca74a969dcd5a31483e79
zsolt <fzs@fwmail.hu>
2020-09-20 20:20:50 +0200

    Test foreign key creation

--- tests/NestedSetTest.php
+++ tests/NestedSetTest.php
@@ -40,6 +40,22 @@ class NestedSetTest extends TestCase
     }
 
     /** @test */
+    public function nested_set_macro_creates_foreign_key()
+    {
+        Schema::create('test_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            NestedSet::addColumns($table);
+        });
+
+        $sm = Schema::getConnection()->getDoctrineSchemaManager();
+
+        $foreignKeys = $sm->listTableForeignKeys('test_categories');
+        $key = $foreignKeys[0];
+
+        $this->assertContains('parent_id', $key->getLocalColumns());
+    }
+
+    /** @test */
     public function drop_nested_set_macro_drops_columns()
     {
         Schema::create('test_categories', function (Blueprint $table) {
